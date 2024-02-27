206cbfca57756e06b139c514e63146519f64d9ad
Osmar Filho <osmarfilho2013@gmail.com>
2024-02-27 07:55:46 -0300

    criando homepage

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwapiService } from '../../../services/swapi.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  filmCount: number = 0;
+  starshipCount: number = 0;
 
+  constructor(private swapiService: SwapiService) {}
+
+  ngOnInit() {
+    this.swapiService.getFilms().subscribe((data) => {
+      this.filmCount = data.count;
+    });
+
+    this.swapiService.getStarships().subscribe((data) => {
+      this.starshipCount = data.count;
+    });
+  }
 }
